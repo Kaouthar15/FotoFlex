ce535088e8434d53e8460ca29f60378eeca76674
Houda ERRANBOUK ZZERBOUH <143433444+HuDa198@users.noreply.github.com>
2024-03-11 23:53:55 +0000

    FotoFlex

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Main from "./components/Main";
 import Acceuil from "./components/Accueil";
 import {Routes,Route} from 'react-router-dom';
+import LearnMore from "./components/learnMore";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
        <Routes>
         <Route path="/" element={<Acceuil />} />
         <Route path="editor" element={<Main />} />
+        <Route path="learn" element={<LearnMore/>}/>
       </Routes> 
 
     </div>
